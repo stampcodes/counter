e11a2a717a8461b05532a2e966d05b5be889980a
Andre <andreafragnelli@gmail.com>
2024-05-15 11:49:41 +0200

    Tolti gli elementi creati sul foglio HTML. Creati elementi del counter tramite la manipolazione del DOM

--- script/main.js
+++ script/main.js
@@ -1,26 +1,56 @@
-const increaseButton = document.querySelector("[data-increase]");
-const increaseButton10 = document.querySelector("[data-increase-10]");
-const decreaseButton = document.querySelector("[data-decrease]");
-const decreaseButton10 = document.querySelector("[data-decrease-10]");
-let screenNumber = document.querySelector("[data-screen-number]");
+// const increaseButton = document.querySelector("[data-increase]");
+// const increaseButton10 = document.querySelector("[data-increase-10]");
+// const decreaseButton = document.querySelector("[data-decrease]");
+// const decreaseButton10 = document.querySelector("[data-decrease-10]");
+// let screenNumber = document.querySelector("[data-screen-number]");
 
 
+
+const decreaseButton10 = document.createElement("button")
+const decreaseButton = document.createElement("button")
+const screenNumber = document.createElement("div")
+const increaseButton = document.createElement("button")
+const increaseButton10 = document.createElement("button")
+
+decreaseButton10.setAttribute("class", "decrease10")
+decreaseButton.setAttribute("class", "decrease")
+screenNumber.setAttribute("class", "screen-number")
+increaseButton.setAttribute("class", "increase")
+increaseButton10.setAttribute("class", "increase10")
+
+decreaseButton10.textContent = "-10"
+decreaseButton.textContent = "-"
+increaseButton.textContent = "+"
+increaseButton10.textContent = "+10"
 screenNumber.textContent = 0;
 
-increaseButton.addEventListener("click", ()=>{
-  screenNumber.textContent++;
+document.querySelector("#counter").append(decreaseButton10)
+document.querySelector("#counter").append(decreaseButton)
+document.querySelector("#counter").append(screenNumber)
+document.querySelector("#counter").append(increaseButton)
+document.querySelector("#counter").append(increaseButton10)
+
+
+
+
+
+
+increaseButton.addEventListener("click", () => {
+  let currentValue = parseInt(screenNumber.textContent)
+  screenNumber.textContent = currentValue + 1;
 })
 
-decreaseButton.addEventListener("click", ()=>{
-  screenNumber.textContent--;
+decreaseButton.addEventListener("click", () => {
+  let currentValue = parseInt(screenNumber.textContent)
+  screenNumber.textContent = currentValue - 1;
 })
 
-increaseButton10.addEventListener("click",()=>{
+increaseButton10.addEventListener("click", () => {
   let currentValue = parseInt(screenNumber.textContent)
   screenNumber.textContent = currentValue + 10;
 })
 
-decreaseButton10.addEventListener("click",()=>{
+decreaseButton10.addEventListener("click", () => {
   let currentValue = parseInt(screenNumber.textContent)
   screenNumber.textContent = currentValue - 10;
 })
